bcfb7fbe26a2759ec8d16de710a13b8686fe09d6
mohammadxxali <ali_mohammad@t-online.de>
2023-07-12 23:03:51 +0200

    add js Navigation edits 3

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -15,3 +15,24 @@ class navigation extends Component {
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll); 
   }
+
+  handleScroll = (event) => {
+    if (window.scrollY === 0 ) {
+      this.setState({ scrolling: false });
+    }
+    else if (window.scrollY > 50) {
+      this.setState({ scrolling: true });
+    }
+  }
+
+  render () {
+
+    return (
+      <nav className={"navigation " + (this.state.scrolling ? "black" : "")} >
+       <NavigationItems />
+      </nav>
+    )
+  }
+} 
+
+export default navigation;
