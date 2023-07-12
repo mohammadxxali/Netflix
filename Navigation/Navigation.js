5d75349a2e40e51e7a89e374bb98f36c3f7abad9
mohammadxxali <ali_mohammad@t-online.de>
2023-07-12 23:02:12 +0200

    add js Navigation edits 2

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,1 +1,17 @@
 import React, { Component } from 'react';
+
+import NavigationItems from '../../components/NavigationItems/NavigationItems';
+
+
+class navigation extends Component {
+ state = {
+   scrolling: false
+ }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll); 
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll); 
+  }
