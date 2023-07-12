4270c3c53ab2a89af29087c8b8b545da1b052343
mohammadxxali <ali_mohammad@t-online.de>
2023-07-12 22:43:36 +0200

    add js layout edits

---
+++ Layout/Layout.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+import Navigation from '../Navigation/Navigation'; 
+import Header from '../../components/Header/Header'; 
+import MovieShowcase from '../MovieShowcase/MovieShowcase'; 
+import MovieOriginals from '../MovieOriginals/MovieOriginals'; 
+import Footer from '../../components/Footer/Footer'; 
+import { BrowserRouter } from "react-router-dom";
