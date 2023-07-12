cac5b1e9819cd8d585d60db654e396a4a8e7b037
mohammadxxali <ali_mohammad@t-online.de>
2023-07-12 22:46:40 +0200

    add js layout edits-2

--- Layout/Layout.js
+++ Layout/Layout.js
@@ -6,3 +6,24 @@ import MovieShowcase from '../MovieShowcase/MovieShowcase';
 import MovieOriginals from '../MovieOriginals/MovieOriginals'; 
 import Footer from '../../components/Footer/Footer'; 
 import { BrowserRouter } from "react-router-dom";
+
+class Layout extends Component {
+
+
+    render() {
+ 
+ 
+       return (
+       <BrowserRouter>
+           <div className="container">
+             <Navigation />
+             <Header />
+             <MovieShowcase />
+             <MovieOriginals />
+             <Footer />
+           </div>
+         </BrowserRouter>)
+    }
+}
+ 
+export default Layout; 
